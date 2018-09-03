2335ce661ee323f04d21c9fdea19e9abd998e470
prattj <42696148+prattj@users.noreply.github.com>
2018-09-03 15:50:34 -0400

    implement simple vue app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Latest Block</h1>
+    <button @click="fetch">Load</button>
+    <hr>
+    <vue-json-pretty
+      :path="'res'"
+      :data="response">
+    </vue-json-pretty>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios'
+import VueJsonPretty from 'vue-json-pretty'
 export default {
   name: 'app',
+  data () {
+    return {
+      response: {}
+    }
+  },
+  methods: {
+    async fetch () {
+      try {
+        const res = await axios.post('http://localhost:4000/graphql', {
+          query: '{ getRecentBlock { id action_cnt } }'
+        })
+        this.response = res.data.data.getRecentBlock
+      } catch (e) {
+        // eslint-disable-next-line
+        console.log('err', e)
+      }
+    }
+  },
   components: {
-    HelloWorld
+    VueJsonPretty
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
